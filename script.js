792f733f9b235b903cfbab6843eb76f41424108b
Fdstephen <21h54.stephen@sjec.ac.in>
2025-01-30 14:01:14 +0530

    Dynamic Job Roles

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const titles = [
+  "Frontend Development",
+  "Backend Development",
+  "Full-Stack Development",
+  "UI/UX Design",
+  "Software Engineering",
+];
+
+let index = 0; // Start index
+
+function changeTitle() {
+  index = (index + 1) % titles.length; // Loop through array
+  document.getElementById("dynamicTitle").textContent = titles[index];
+}
+
+setInterval(changeTitle, 2000);
